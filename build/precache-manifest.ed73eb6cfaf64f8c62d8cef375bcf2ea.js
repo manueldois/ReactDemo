b4e219f725fb76ac01942bbf2aee7013cebc7028
Manuel <manuelsilvadois@gmail.com>
2019-10-01 20:07:57 +0100

    Pushing build folder for gitPages

---
+++ build/precache-manifest.ed73eb6cfaf64f8c62d8cef375bcf2ea.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a96b75edfcbeb7f7be8b513c982bf335",
+    "url": "/ReactDemo/index.html"
+  },
+  {
+    "revision": "e9b4671ff89cfd2e685b",
+    "url": "/ReactDemo/static/css/4.b0218a02.chunk.css"
+  },
+  {
+    "revision": "15c798578fc5fc74b789",
+    "url": "/ReactDemo/static/css/6.8701267b.chunk.css"
+  },
+  {
+    "revision": "c287bccf6b80592a18ff",
+    "url": "/ReactDemo/static/css/7.32035dd5.chunk.css"
+  },
+  {
+    "revision": "bbc765ef7e09ccbbb7d9",
+    "url": "/ReactDemo/static/css/8.eed09fc2.chunk.css"
+  },
+  {
+    "revision": "cc31101aeb11a3cb21d6",
+    "url": "/ReactDemo/static/css/9.6ee85eb9.chunk.css"
+  },
+  {
+    "revision": "c7a3827e0fa519d40ddb",
+    "url": "/ReactDemo/static/css/main.c682128b.chunk.css"
+  },
+  {
+    "revision": "6d33d6d4c37d5e6a580a",
+    "url": "/ReactDemo/static/js/2.9021045a.chunk.js"
+  },
+  {
+    "revision": "843ff14a8113e8cacea6",
+    "url": "/ReactDemo/static/js/3.0614ecb2.chunk.js"
+  },
+  {
+    "revision": "e9b4671ff89cfd2e685b",
+    "url": "/ReactDemo/static/js/4.59dbbe1d.chunk.js"
+  },
+  {
+    "revision": "c2741e96272426625137",
+    "url": "/ReactDemo/static/js/5.a5d85570.chunk.js"
+  },
+  {
+    "revision": "15c798578fc5fc74b789",
+    "url": "/ReactDemo/static/js/6.93bbcd82.chunk.js"
+  },
+  {
+    "revision": "c287bccf6b80592a18ff",
+    "url": "/ReactDemo/static/js/7.2bee523b.chunk.js"
+  },
+  {
+    "revision": "bbc765ef7e09ccbbb7d9",
+    "url": "/ReactDemo/static/js/8.8b158a6b.chunk.js"
+  },
+  {
+    "revision": "cc31101aeb11a3cb21d6",
+    "url": "/ReactDemo/static/js/9.b92e84c2.chunk.js"
+  },
+  {
+    "revision": "c7a3827e0fa519d40ddb",
+    "url": "/ReactDemo/static/js/main.16edf861.chunk.js"
+  },
+  {
+    "revision": "16e00c4cdf69720df62f",
+    "url": "/ReactDemo/static/js/runtime~main.2f660526.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ReactDemo/static/media/logo.5d5d9eef.svg"
+  }
+]);
